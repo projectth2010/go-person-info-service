daca9f8be77b70d50c6397c2a733bb2ce37b76b8
Boonsit.R <projectth2010@gmail.com>
2025-12-16 06:37:22 +0700

    Update route.go

--- routes/route.go
+++ routes/route.go
@@ -8,7 +8,27 @@ import (
 )
 
 func UserRoutes(r *gin.Engine) {
-	r.POST("/register", controllers.RegisterUser)
-	r.POST("/login", controllers.LoginUser)
-	r.GET("/user", middleware.JWTAuthMiddleware(), controllers.GetUserAccount)
+	// Public routes
+	api := r.Group("/api/v1")
+	{
+		// Auth routes
+		auth := api.Group("/auth")
+		{
+			auth.POST("/register", controllers.RegisterUser)
+			auth.POST("/login", controllers.LoginUser)
+		}
+
+		// Protected routes
+		users := api.Group("/users")
+		users.Use(middleware.JWTAuthMiddleware())
+		{
+			// User profile management
+			users.GET("/me", controllers.GetUserAccount)
+			users.PUT("/me", controllers.UpdateUserProfile)
+			users.DELETE("/me", controllers.DeleteUserAccount)
+			
+			// Password management
+			users.POST("/me/change-password", controllers.ChangePassword)
+		}
+	} 
 }
