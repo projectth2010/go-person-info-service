02ef683b53a4ae8859cf31cba8d1247903db69c6
Boonsit.R <projectth2010@gmail.com>
2025-12-16 07:58:10 +0700

    Update db.go

--- config/db.go
+++ config/db.go
@@ -2,60 +2,54 @@
 package config
 
 import (
-    "context"
-    "fmt"
-    "os"
-    "time"
+	"context"
+	"fmt"
+	"os"
+	"time"
 
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var DB *mongo.Database
-
-func getMongoURI() string {
-    host := os.Getenv("MONGODB_HOST")
-    if host == "" {
-        host = "localhost"
-    }
-    port := os.Getenv("MONGODB_PORT")
-    if port == "" {
-        port = "27017"
-    }
-    return fmt.Sprintf("mongodb://%s:%s", host, port)
-}
+var (
+	DB             *mongo.Database
+	UserCollection *mongo.Collection
+)
 
 func ConnectDB() error {
-    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-    defer cancel()
+	// Get MongoDB URI from environment or use default
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		mongoURI = "mongodb://localhost:27017"
+	}
 
-    // Get MongoDB URI from environment or use default
-    mongoURI := os.Getenv("MONGODB_URI")
-    if mongoURI == "" {
-        mongoURI = getMongoURI()
-    }
+	// Get database name from environment or use default
+	dbName := os.Getenv("MONGODB_NAME")
+	if dbName == "" {
+		dbName = "person_info_service"
+	}
 
-    // Set client options
-    clientOptions := options.Client().ApplyURI(mongoURI)
+	// Set client options
+	clientOptions := options.Client().ApplyURI(mongoURI)
 
-    // Connect to MongoDB
-    client, err := mongo.Connect(ctx, clientOptions)
-    if err != nil {
-        return fmt.Errorf("failed to connect to MongoDB: %w", err)
-    }
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-    // Check the connection
-    err = client.Ping(ctx, nil)
-    if err != nil {
-        return fmt.Errorf("failed to ping MongoDB: %w", err)
-    }
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return fmt.Errorf("failed to connect to MongoDB: %w", err)
+	}
 
-    // Get database name from environment or use default
-    dbName := os.Getenv("MONGODB_NAME")
-    if dbName == "" {
-        dbName = "person_info_service"
-    }
+	// Check the connection
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("failed to ping MongoDB: %w", err)
+	}
 
-    DB = client.Database(dbName)
-    return nil
+	// Set global database and collection variables
+	DB = client.Database(dbName)
+	UserCollection = DB.Collection("users")
+
+	return nil
 }
